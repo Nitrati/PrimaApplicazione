75f3586ed9d8aade99119cebc06efe0aa53c5d48
NITRATI MATTEO <st10924@iismarconipieralisi.it>
2026-03-14 09:22:36 +0100

    first test of code in Java

--- app/src/main/java/com/example/primaapplicazione/MainActivity.java
+++ app/src/main/java/com/example/primaapplicazione/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.primaapplicazione;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +15,45 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //dichiarazione degli oggetti utilizzati xml -> java
+    Button btnMostra;
+    ImageView ImgFotoScuola;
+    TextView txtMessaggio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            //qui richiamo il logout con "findViewById(R.id.btnLogout)"
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        //il codice va inserito qui!!
+        //IL CODICE NORMALE VA INERITO QUA...
+
+        //1.inizializzazione degli oggetti
+        txtMessaggio = findViewById(R.id.txtMessaggio);
+        btnMostra = findViewById(R.id.btnMostra);
+        ImgFotoScuola = findViewById(R.id.ImgFotoScuola);
+
+        //2.assegnare un evento al Button
+        btnMostra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //variabile di appoggio per il messaggio
+                String messaggio = txtMessaggio.getText().toString();
 
 
 
 
 
-
-
+                Toast.makeText(MainActivity.this, messaggio, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-
 //funzioni e metodi, vanno inseriti qui
-
-
-
-
 
 
 }
